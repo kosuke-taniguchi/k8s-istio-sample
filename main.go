66baed730810981cafbc0a13a3667fb6d8752178
kosuke-taniguchi <applekkttegg@yahoo.co.jp>
2023-10-18 11:28:26 +0900

    added ConfigMap and Namespace

--- main.go
+++ main.go
@@ -8,17 +8,21 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-
 func main() {
 	e := echo.New()
-  
+
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-  
+
 	e.GET("/", func(c echo.Context) error {
 		msg := os.Getenv("MSG")
 		return c.JSON(http.StatusOK, msg)
 	})
-  
+
+	e.GET("/api-version", func(c echo.Context) error {
+		msg := os.Getenv("API_VERSION")
+		return c.JSON(http.StatusOK, msg)
+	})
+
 	e.Logger.Fatal(e.Start(":8080"))
-  }
+}
